e6ff0301128a0678f2975e81cc84ed940c55888c
Archana Balakrishna Lokhande <145834338+Archana-547@users.noreply.github.com>
2023-09-26 01:01:16 +0300

    I have completed homework

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("orange!");
+        System.out.println("white!");
+        System.out.println("green!");
+    }
+}
---
+++ src/NewClass.java
@@ -0,0 +1,8 @@
+public class NewClass {
+
+    public static void main(String[] args) {
+        int favoriteNumber = 44;
+        System.out.println("My favorite number is: " + favoriteNumber);
+
+    }
+}
